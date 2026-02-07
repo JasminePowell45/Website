678ddc0053eb0f3629e4803adeac55cd3eb6b433
JasminePowell45 <jypowell45@gmail.com>
2026-02-07 17:55:17 +0000

    Update Javascript1.js

--- Javascript1.js
+++ Javascript1.js
@@ -1,28 +1,10 @@
-
-document.addEventListener('DOMContentLoaded' , () => {
-const track = document.querySelector('.carousel-track');
-const slides = Array.from(track.children).filter(el => el.nodeType === 1);
-const nextButton = document.querySelector('.carousel-btn.next');
-
-let currentIndex = 0;
-
-function updateSlidePosition() {
-    const slideWidth = slides[0].getBoundingClientRect().width;
-    track.style.transform =  `translateX(-${currentIndex * slideWidth}px)`;
-}
-
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slides.length;
-    updateSlidePosition();
-});
-window.addEventListener('resize' , updateSlidePosition);
-});
-
-const toggle = document.querySelector('.menu-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+const menuToggle = document.querySelector('.menu-toggle');
 const navList = document.querySelector('.nav-list');
 
-toggle.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => {
     navList.classList.toggle('active');
+});
 });
 
 
