5d6e9691c6bc9e987ea5f9f7c514dcc650f85bea
JasminePowell45 <jypowell45@gmail.com>
2026-02-20 01:07:22 +0000

    Update Javascript1.js

--- Javascript1.js
+++ Javascript1.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 const menuToggle = document.querySelector('.menu-toggle');
-const navList = document.querySelector('.header-nav');
+const navList = document.querySelector('.header-nav .nav-list');
 
-if (menuToggle && headernav) {
-    menuToggle.addEventListener('click', () => {
-    headerNav.classList.toggle('active');
+menuToggle.addEventListener('click', () => {
+navList.classList.toggle('active');
 });
-}
 });
 
 
